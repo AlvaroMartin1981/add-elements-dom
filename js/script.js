76e88207dc415e76485bbb5975d8fc4e198f16c7
titopope@hotmail.com <titopope@hotmail.com>
2023-11-09 21:20:12 +0100

    añadido css

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
  // Aquí tu código
+
+
+ const lista = document.getElementById('lista');
+ const agregar = document.getElementById('agregar');
+
+ agregar.addEventListener('click', function(){
+
+    let cartel = prompt('Agregar elemento');
+
+    
+
+     let cartelOne = document.createElement('li');
+     cartelOne.innerText = cartel;
+     lista.appendChild(cartelOne);
+
+    })
+
+
+
