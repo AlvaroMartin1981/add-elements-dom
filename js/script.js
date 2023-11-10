130ccab4f3e7fd11828b1408139c8161b168c42e
titopope@hotmail.com <titopope@hotmail.com>
2023-11-10 20:26:57 +0100

    ejercicio hecho en clase

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
  // Aquí tu código
 
+ const agregar = document.getElementById('agregar')  
+ const lista = document.getElementById('lista') 
 
- const lista = document.getElementById('lista');
+
+ agregar.addEventListener('click', function(){
+    const pregunta = prompt('introducir cosa')
+    lista.insertAdjacentHTML('beforebegin',`<li>${pregunta}</li>`)  
+ })
+
+
+ /*const lista = document.getElementById('lista');
  const agregar = document.getElementById('agregar');
 
  agregar.addEventListener('click', function(){
@@ -14,7 +23,34 @@
      cartelOne.innerText = cartel;
      lista.appendChild(cartelOne);
 
-    })
+    })*/
+
+   /*const agregar = document.getElementById('agregar')  
+   const lista = document.getElementById('lista') 
 
 
+   agregar.addEventListener('click', function(){
+      const pregunta = prompt('introducir cosa')
+      const li = document.createElement('li');
+      li.textContent = pregunta;
+      lista.appendChild(li);
+   })*/
+   /*
+   const agregar = document.getElementById('agregar')  
+   const lista = document.getElementById('lista') 
+   const checkImput = document.getElementById('checkImput')
 
+   agregar.addEventListener('click', function() {
+      const pregunta = prompt('introducir cosa')
+      const li = document.createElement('li');
+      li.innerHTML = `<input type="checkbox">${pregunta}</input>`
+      lista.appendChild(li);
+   })
+
+checkImput.addEventListener('change', function() {
+   if(this.checed) {
+      console.log('isChecked')
+   } else {
+      console.log('noChecked')
+   }
+})*/
